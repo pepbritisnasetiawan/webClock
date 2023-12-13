0a6f69f5c38de2b323d6c9f1a3125001ad1be8de
pepbritisnasetiawan <pepbritisnasetiawan@gmail.com>
2023-12-13 20:09:24 +0700

    add HtmlWebpckPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,5 +7,44 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    mode: 'production'
+    mode: 'production',
+    module: {
+        rules: [
+            /* style and css loader */
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+
+            /* babel loader */
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    /** plugin */
+    plugins: [
+        /** HTML webpack plugin */
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            filename: 'index.html',
+        }),
+    ],
 };
